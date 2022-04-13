e0253cc8803c659622ca7c9e15e81c52aca55ec1
jawherr <jawa.kalel@gmail.com>
2022-04-13 22:50:40 +0100

    first commit

---
+++ src/main/java/com/camunda/bpm/demo/camunda_bpm_boot_pfe/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.camunda.bpm.demo.camunda_bpm_boot_pfe;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "camunda-bpm-boot-pfe"; // BPMN Process ID
+
+}
